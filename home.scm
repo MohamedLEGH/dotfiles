a1dc272f2ca9a57ceb97f2690535ac981b96112e
Mohamed Amine LEGHERABA <mlegheraba@protonmail.com>
2022-10-14 03:05:27 +0200

    update home

--- home.scm
+++ home.scm
@@ -6,12 +6,25 @@
   (gnu home services ssh)
   (gnu services)
   (gnu packages)
+  (gnu packages tex)
+  (gnu packages base)
   (gnu packages admin)
   (gnu packages shells)
   (gnu packages telegram)
   (gnu packages package-management)
   (gnu packages freedesktop)
-  (gnu packages ssh))
+  (gnu packages libreoffice)
+  (gnu packages video)
+  (gnu packages ssh)
+  (gnu packages rust)
+  (gnu packages crates-io)
+  (gnu packages python)
+  (gnu packages node)
+  (gnu packages irc)
+  (gnu packages gimp)
+  (gnu packages disk)
+  (gnu packages pdf)
+  (gnu packages imagemagick))
 
 (define %home-files
   `(
@@ -28,10 +41,29 @@
 (home-environment
  (packages 
    (list 
+     vlc
+     shotcut
+     openshot
+     texlive-base
+     gnu-make
      openssh
-     htop 
-     flatpak xdg-desktop-portal xdg-desktop-portal-gtk 
-     zsh))
+     htop
+     libreoffice
+     flatpak
+     zsh
+     rust 
+     rust-cargo-0.53
+     python
+     node
+     weechat ; irc chat
+     gimp ; pictures editing
+     obs ; recording
+     testdisk ; repair disk
+     xournalpp ; edit pdf
+     poppler ; combine pdf
+     imagemagick ; pdf to jpeg
+     nix ; package manager
+))
  (services
   (list
     (service home-files-service-type %home-files)
